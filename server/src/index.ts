94bdd9d7604122470f314f9c14b101616cd2597e
kinwong <kinhangwong@gmail.com>
2019-01-13 22:44:08 +1100

    Added routes; Removed compiled artifacts.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,6 @@
-import express from "express";
-const app = express();
-const port = 8080; // default port to listen
+import app from './app';
+const PORT = 8080;
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
-
-// start the Express server
-app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(PORT, () => {
+    console.log( `server started at http://localhost:${ PORT }`);
+});
