cd808bf2f01196f19beaa22aa10a3f57ee118fe3
kinwong <kinhangwong@gmail.com>
2019-01-13 23:19:59 +1100

    common.... don't know how.

---
+++ common/src/text-to-speech/contract.ts
@@ -0,0 +1,7 @@
+export type GenderType = 'SSML_VOICE_GENDER_UNSPECIFIED'|'MALE'|'FEMALE'|'NEUTRAL';
+// export type 
+
+// SSML_VOICE_GENDER_UNSPECIFIED	An unspecified gender. In VoiceSelectionParams, this means that the client doesn't care which gender the selected voice will have. In the Voice field of ListVoicesResponse, this may mean that the voice doesn't fit any of the other categories in this enum, or that the gender of the voice isn't known.
+// MALE	A male voice.
+// FEMALE	A female voice.
+// NEUTRAL	A gender-neutral voice.
