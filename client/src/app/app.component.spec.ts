44c5a140d0e9f66052ea0273993fd0ba444cb146
kinwong <kinhangwong@gmail.com>
2019-02-24 11:56:01 +1100

    Renamed all reference of 'abrsm-piano-practice' to 'helen-student-corner'.

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'abrsm-piano-practice'`, () => {
+  it(`should have as title 'helen-student-corner'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('abrsm-piano-practice');
+    expect(app.title).toEqual('helen-student-corner');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to abrsm-piano-practice!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to helen-student-corner!');
   });
 });
