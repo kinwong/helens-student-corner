5540ffb5b4be975b86078d1cad9f4fdddb9ca11a
kinwong <kinhangwong@gmail.com>
2019-07-28 22:17:55 +1000

    material done.

--- client/src/app/material.module.ts
+++ client/src/app/material.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatDividerModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatListModule,
-  MatProgressBarModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatTableModule,
-  MatToolbarModule,
-  MatStepperModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
